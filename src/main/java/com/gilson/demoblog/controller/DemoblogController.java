07b9687475f4302678bdc48068fb2ad9b7a262ff
Gilson <gilson.costa@dce.ufpb.br>
2020-04-15 19:26:56 -0300

    Salvando um novo post

--- src/main/java/com/gilson/demoblog/controller/DemoblogController.java
+++ src/main/java/com/gilson/demoblog/controller/DemoblogController.java
@@ -1,13 +1,18 @@
 package com.gilson.demoblog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gilson.demoblog.model.Post;
 import com.gilson.demoblog.service.DemoblogService;
@@ -32,6 +37,21 @@ public class DemoblogController {
 		Post post = this.demoblogService.findById(id);
 		mv.addObject("post", post);
 		return mv;
+	}
+	
+	@RequestMapping(value="/newpost", method=RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+	
+	@RequestMapping(value="/newpost", method=RequestMethod.POST)
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+			return "redirect:/newpost";
+		}
+		post.setData(LocalDate.now());
+		this.demoblogService.save(post);
+		return "redirect:/posts";
 	}
 	
 }
